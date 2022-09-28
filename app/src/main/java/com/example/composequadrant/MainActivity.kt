c81092def7700b7c526f8e26614641a355aa804e
Amadeusz Lisiecki <amadeusz.lisiecki.1993@gmail.com>
2022-09-28 23:35:31 +0200

    Dodane dopełnienie w ćwiartkach.

--- app/src/main/java/com/example/composequadrant/MainActivity.kt
+++ app/src/main/java/com/example/composequadrant/MainActivity.kt
@@ -3,13 +3,14 @@ package com.example.composequadrant
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.composequadrant.ui.theme.ComposeQuadrantTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +23,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    Square()
                 }
             }
         }
@@ -30,14 +31,54 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun SmallSquare(
+    title: String,
+    description: String,
+    modifier: Modifier
+) {
+    Column(modifier = modifier
+        .fillMaxSize()
+        .padding(16.dp)
+    ) {
+        Text(text = title)
+        Text(text = description)
+    }
+}
+
+@Composable
+fun Square() {
+    Column {
+        Row(Modifier.weight(1f)) {
+            SmallSquare(
+                "Tekst komponowany",
+                "Wyświetla tekst i postępuje zgodnie z wytycznymi Material Design.",
+                Modifier.weight(1f)
+            )
+            SmallSquare(
+                "Obraz komponowany",
+                "Tworzy element do komponowania, który układa i rysuje dany obiekt klasy Painter.",
+                Modifier.weight(1f)
+            )
+        }
+        Row(Modifier.weight(1f)) {
+            SmallSquare(
+                "Wiersz komponowany",
+                "Układ, który można komponować, umieszcza swoje elementy podrzędne w kolejności poziomej.",
+                Modifier.weight(1f)
+            )
+            SmallSquare(
+                "Kolumna komponowana",
+                "Układ, który można komponować, umieszcza swoje elementy podrzędne w kolejności pionowej.",
+                Modifier.weight(1f)
+            )
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     ComposeQuadrantTheme {
-        Greeting("Android")
+        Square()
     }
 }
